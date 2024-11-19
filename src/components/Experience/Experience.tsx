f513bd6716d19c2b1bd8bd58059e0d61b60adf81
ashutosh04k <kumarasutosh2014@gmail.com>
2024-11-19 23:41:34 +0530

    Experience section Added 2

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -12,11 +12,6 @@ export function Experience() {
             <ScrollAnimation animateIn="flipInX">
               <div className="project">
                 <header>
-                  {/* <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"> 
-                    <title>Folder</title> 
-                    <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
-                    
-                  </svg> */}
                   <div className="Company-logo">
 
                   <a href="https://app.interviewintelligence.in/login" target="_blank" rel="noreferrer">
@@ -37,7 +32,7 @@ export function Experience() {
                   </div>
                 </header>
                 <div className="body">
-                  <h3>Tawgl</h3>
+                  <h3>Tawgl - Internship</h3>
                   <p>
                   Engineered a chat feature for interviews, boosting real-time communication and increasing user engagement by 30%, while improving feedback collection efficiency by 40%.</p>
                   <p>Implemented Notification service using WebSocket, leading to a 50% boost in client engagement through real-time server-frontend communication.</p>
